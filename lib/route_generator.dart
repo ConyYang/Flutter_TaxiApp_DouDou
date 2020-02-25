046f8338a4f98f2c603a173263552d11ef691b05
Yang Yubei <yangyubei0218@gmail.com>
2020-02-25 22:37:48 +0800

    HomePage, LoginPage, WelcomePage, ProfilePage,

---
+++ lib/route_generator.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:singtaxi/main.dart';
+
+class RouteGenerator{
+  static Route<dynamic> generateRoute(RouteSettings settings){
+  final args = settings.arguments;
+  switch(settings.name)
+  {
+    case '/': //home route
+      return MaterialPageRoute(builder: (_) => StartPage());
+
+    case '/Login':
+          return MaterialPageRoute(builder: (_) => LoginPage());
+
+    case '/Welcome':
+      return MaterialPageRoute(builder: (_) => WelcomePage());
+    case '/profile':
+      return MaterialPageRoute(builder: (_) => profilePage());
+    //case '/MyHomePage':
+     // return MaterialPageRoute(builder: (_) => MyHomePage());
+    default:
+      return _errorRoute();
+    }
+  }
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Error'),
+          ),
+          body: Center(
+            child: Text('ERROR'),
+          )
+      );
+    });
+  }
+
+}
