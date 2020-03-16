31c1c337b427652ea834ec903d066e6b530dd4ed
NgJoshuaJeremiahNTU <57743211+NgJoshuaJeremiahNTU@users.noreply.github.com>
2020-03-16 16:37:11 +0800

    adding anon login

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:singtaxi/screens/authenticate/LoginPage.dart';
+import 'package:singtaxi/screens/authenticate/sign_in.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: LoginPage(),
+    );
+  }
+}
---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:singtaxi/services/auth.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[100],
+        elevation: 0.0,
+        title: Text('Sign in to DouDou'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: RaisedButton(
+          child:Text('Sign in anon'),
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('error signing in');
+            }
+            else
+              {
+                print('signed in');
+                print(result.uid);
+              }
+          }
+        )
+      )
+    );
+  }
+}
